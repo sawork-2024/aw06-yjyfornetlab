7f7b2498d5a0d008db27673f9e569fc61b5fcab8
yjy <3089866121@qq.com>
2024-05-15 11:17:29 +0800

    add new files

--- webpos-discovery-server/src/main/java/com/example/discovery/DiscoveryServerApplication.java
+++ webpos-discovery-server/src/main/java/com/example/discovery/DiscoveryServerApplication.java
@@ -4,9 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-/**
- * @author Maciej Szarlinski
- */
 @SpringBootApplication
 @EnableEurekaServer
 public class DiscoveryServerApplication {
